54b2024a4c8bf3ed46d5a081cd564cfb0dce6e59
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-18 12:52:47 -0400

    commit tree 2b0bdccd3a286df44a2c2fd0b7827ab932501ed7

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleFilter(e) {
+    setSelectedCategory(e.target.value);
+    console.log(e.target.value);
+  }
+
+  let filteredList = items.filter((item) => {
+    console.log(item.category);
+    return selectedCategory === "All" || item.category === selectedCategory
+      ? true
+      : false;
+  });
+  console.log(filteredList);
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredList.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +36,7 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+// selectedCategory === "All" || items.category === selectedCategory ? true : false
+
+// .filter()=>{ } MUST ALWAYS HAVE A RETURN!!!!
